be6b3e6c31f699f9091541b5a4739beb53c0c842
Anoofc <anoof7262@gmail.com>
2024-04-20 22:54:00 +0400

    tested and fixed bugs on the three LED strips

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #define DEBUG 0
 
-#define STRIP_1_PIN 2
-#define STRIP_2_PIN 3
-#define STRIP_3_PIN 4 
-#define STRIP_4_PIN 5
-#define STRIP_5_PIN 6
+#define STRIP_1_PIN 19
+#define STRIP_2_PIN 18
+#define STRIP_3_PIN 26
+#define STRIP_4_PIN 21
+#define STRIP_5_PIN 22
 
 #define STRIP_1_LED_COUNT 30
 #define STRIP_2_LED_COUNT 30
@@ -13,8 +13,8 @@
 #define STRIP_5_LED_COUNT 30
 
 #define STRIP_1_DELAY 50
-#define STRIP_2_DELAY 50
-#define STRIP_3_DELAY 50
+#define STRIP_2_DELAY 10
+#define STRIP_3_DELAY 70
 #define STRIP_4_DELAY 50
 #define STRIP_5_DELAY 50
 
@@ -28,6 +28,8 @@
 #define STRIP_3_COLOR_2 0, 255, 0
 #define STRIP_3_COLOR_3 0, 0, 255
 
+#define THEATERCHASE_MODE_UPDATE_TIME 5000
+
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
@@ -38,6 +40,8 @@ uint32_t strip3_update_time = 0;
 uint32_t strip4_update_time = 0;
 uint32_t strip5_update_time = 0;
 
+uint8_t colorWipe_mode = 0; 
+uint8_t theaterChase_mode = 0;
 
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(STRIP_1_LED_COUNT, STRIP_1_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(STRIP_2_LED_COUNT, STRIP_2_PIN, NEO_GRB + NEO_KHZ800);
@@ -56,73 +60,90 @@ void colorWipe(uint8_t r, uint8_t g, uint8_t b, uint16_t wait) {
     i++;
     if (i >= STRIP_1_LED_COUNT) {
       i = 0;
+      colorWipe_mode++; if (colorWipe_mode > 2) { colorWipe_mode = 0; }
     }
 }
 
 void colorWipe_Run() {
-  static uint8_t mode = 0;
-  if( mode == 0) {
+ 
+  if( colorWipe_mode == 0) {
     colorWipe(255, 0, 0, STRIP_1_DELAY);
-  } else if (mode == 1) {
+  } else if (colorWipe_mode == 1) {
     colorWipe(0, 255, 0, STRIP_1_DELAY);
-  } else if (mode == 2) {
+  } else if (colorWipe_mode == 2) {
     colorWipe(0, 0, 255, STRIP_1_DELAY);
   }
-  mode++; if (mode > 2) { mode = 0; }
+  
 }
 
 
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(uint16_t wait) {
-  if (millis() - strip2_update_time < wait) {
-    return;
-  } 
-    strip2_update_time = millis();
+void rainbow(int wait) {
   
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+  static long firstPixelHue = 0; // Keep track of the hue
+
+  // Check if 'wait' milliseconds have passed since the last update
+  if (millis() - strip2_update_time >= wait) {
+    // Update the last update time
+    strip2_update_time = millis();
+
+    // Update the rainbow
     strip2.rainbow(firstPixelHue);
-    strip2.show(); // Update strip with new contents
+
+    // Show the new contents on the strip
+    strip2.show();
+
+    // Update the hue for the next iteration
+    firstPixelHue += 256;
+    if (firstPixelHue >= 5*65536) {
+      firstPixelHue = 0; // Reset the hue if it's out of range
+    }
   }
 }
 
 // THEATER CHASE
 
 void theaterChase(uint8_t red, uint8_t green, uint8_t blue, uint32_t wait) {
-  static int a = 0;
+    static int a = 0;
   static int b = 0;
   static int c = 0;
-  static uint32_t lastUpdateTime = 0;
 
-  if (millis() - lastUpdateTime < wait) {
-    return;
-  }
+  // Check if 'wait' milliseconds have passed since the last update
+  if (millis() - strip3_update_time >= wait) {
+    // Update the last update time
+    strip3_update_time = millis();
 
-  lastUpdateTime = millis();
+    strip3.clear(); // Set all pixels in RAM to 0 (off)
+    for(c; c<strip3.numPixels(); c += 3) {
+      strip3.setPixelColor(c, strip3.Color(red,green,blue)); // Set pixel 'c' to value 'color'
+    }
+    strip3.show(); // Update strip with new contents
 
-  for (uint8_t i = 0; i < 3; i++) {
-    if (a < 10) {
-      if (b < 3) {
-        strip3.clear();
-        for (c = b; c < STRIP_3_LED_COUNT; c += 3) {
-          strip3.setPixelColor(c, strip3.Color(red, green, blue));
-        }
-        strip3.show();
-        b++;
-      } else { b = 0; a++; }
-    } else { a = 0; }
+    c = ++b;
+    if (b > 2) {
+      b = 0;
+      if (++a > 9) {
+        a = 0;
+      }
+    }
   }
 }
 
 void theaterChase_Run() {
-  static uint8_t mode = 0;
-  if (mode == 0) {
+  static uint32_t modeUpdate_time = 0;
+  if (theaterChase_mode == 0) {
     theaterChase(STRIP_3_COLOR_1, STRIP_3_DELAY);
-  } else if (mode == 1) {
+  } else if (theaterChase_mode == 1) {
     theaterChase(STRIP_3_COLOR_2, STRIP_3_DELAY);
-  } else if (mode == 2) {
+  } else if (theaterChase_mode == 2) {
     theaterChase(STRIP_3_COLOR_3, STRIP_3_DELAY);
   }
+  if (millis() - modeUpdate_time > THEATERCHASE_MODE_UPDATE_TIME) {
+    modeUpdate_time = millis();
+    theaterChase_mode++; if (theaterChase_mode > 2) { theaterChase_mode = 0; }
+  }
+
 }
 
 void stripBegin() {
